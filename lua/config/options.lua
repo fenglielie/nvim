bc1fd8dee43b52715f513e793171b89598dba4aa
fenglielie <fenglielie@qq.com>
2025-06-27 16:51:03 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
 
-vim.opt.listchars = "tab:▷ ,trail:·,nbsp:␣"
+opt.listchars = "tab:▷ ,trail:·,nbsp:␣"
+
+opt.conceallevel = 0
+
+opt.wrap = true
+opt.linebreak = true
+opt.showbreak = "↪ "
+
+opt.relativenumber = false
